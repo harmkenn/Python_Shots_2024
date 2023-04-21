7f1d3267daf45959579a47c26f5fef371a830b5e
Ken Harmon <harmkenn@isu.edu>
2023-04-21 10:29:40 +0000

    update

--- apps/a_deflection.py
+++ apps/a_deflection.py
@@ -14,7 +14,7 @@ def app():
         if 'a_lat' not in st.session_state: st.session_state.a_lat = -30
         lat = st.session_state.a_lat
         lat = float(st.text_input('Latitude', lat))
-        st.session_state['a_lat'] = lat
+        st.session_state.a_lat = lat
         lon = float(st.text_input('Longitude',30))
         back = zf.LL2MGRS(lat,lon)
         ip = zf.LL2MGRS(lat+.1,lon+.1)
