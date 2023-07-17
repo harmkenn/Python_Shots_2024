659c6b97688be2a5f4bc1aad091d363327bd22f2
Ken Harmon <harmkenn@gmail.com>
2023-07-17 19:51:13 +0200

    getting closer

--- apps/h_celgrid.py
+++ apps/h_celgrid.py
@@ -12,10 +12,10 @@ def app():
     options = ['the Sun','the Moon','Polaris','Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
     selection = st.selectbox("Celestial Object", options)
     st.markdown(f'Get your position from your direction and altitude to {selection}.')
-    if 'h_time' not in st.session_state: st.session_state['h_time'] = datetime.now(timezone.utc)
+    if 'h_time' not in st.session_state: st.session_state['h_time'] = ephem.Date(datetime.now(timezone.utc))
     h_time = st.session_state['h_time']
     if st.button('Now'):
-        h_time = datetime.now(timezone.utc)
+        h_time = ephem.Date(datetime.now(timezone.utc))
     h_time = st.text_input('UTC Time',h_time)
     st.session_state['h_time'] = h_time
     st.write(str(h_time)+'UTC')
@@ -23,42 +23,40 @@ def app():
     h_az = st.number_input(f'Azimuth degrees to {selection}: ', 0,360,45)
     h_alt = st.number_input(f'Altitude degrees to {selection}: ', 0,90,45)
 
+    observer = ephem.Observer()
+    observer.lat = '0'  # Example latitude (London, United Kingdom)
+    observer.lon = '0'
     if selection == 'the Sun': 
-        # Define the observed altitude and azimuth of the Sun
-        observed_altitude = np.radians(45.0/180*np.pi)  # degrees to radians
-        observed_azimuth = np.radians(190.0/180*np.pi)  # degrees to radians
-
-        # Create an Observer object
-        observer = ephem.Observer()
-        observer.lat = '0'  # Set observer's latitude (in this case, 0 degrees)
-        observer.lon = '0'  # Set observer's longitude (in this case, 0 degrees)
-        observer.elev = 0  # Set observer's elevation (in this case, sea level)
-        observer.date = ephem.now()  # Set the current date and time
-
-        # Compute the observer's location based on the observed altitude and azimuth of the Sun
-        sun = ephem.Sun()
-        sun_altitude = observed_altitude
-        sun_azimuth = observed_azimuth
-        sun.compute(observer)
-        sun_altitude_diff = sun.alt - sun_altitude
-        sun_azimuth_diff = sun.az - sun_azimuth
-
-        # Update the observer's position based on the computed differences
-        observer.lat += sun_altitude_diff
-        observer.lon += sun_azimuth_diff
-
-        # Extract the observer's latitude and longitude
-        observer_latitude = np.degrees(observer.lat)
-        observer_longitude = np.degrees(observer.lon)
-
-        # Print the computed observer's location
-        st.write("Computed Observer's Latitude:", observer_latitude)
-        st.write("Computed Observer's Longitude:", observer_longitude)
-
-    elif selection == 'the Moon': out = 'Moon is up'
+        cel = ephem.Sun()
+    elif selection == 'the Moon': 
+        cel = ephem.Moon()
+    elif selection == 'Polaris': 
+        cel = ephem.star('Polaris') # https://theskylive.com/sky/stars/polaris-alpha-ursae-minoris-star
+    elif selection == 'Mercury': 
+        cel = ephem.Mercury() # https://theskylive.com/where-is-mercury
+    elif selection == 'Venus': 
+        cel = ephem.Venus()
+    elif selection == 'Mars': 
+        cel = ephem.Mars()
+    elif selection == 'Jupiter': 
+        cel = ephem.Jupiter()
+    elif selection == 'Saturn': 
+        cel = ephem.Saturn()
+    elif selection == 'Uranus': 
+        cel = ephem.Uranus()
+    elif selection == 'Neptune': 
+        cel = ephem.Neptune()
     else: out = 'Something else'
     
-   
+    cel.compute(observer)
+    cel_azimuth = float(cel.az)*180/np.pi
+    cel_altitude = float(cel.alt)*180/np.pi
+    st.write(f"{selection}'s Azimuth from 0,0:", cel_azimuth)
+    st.write(f"{selection}'s Altitude from 0,0:", cel_altitude)
+    cel_dist = 40050*(90-cel_altitude)/360
+    st.write(cel_dist)
+    sub_cel = zf.polar2LL(0,0,cel_azimuth,cel_dist)
+    st.write(f"{selection}'s Sub-Latitude: ", sub_cel[0], f"{selection}'s Sub-Longitude: ", sub_cel[1] )
     
 
 
