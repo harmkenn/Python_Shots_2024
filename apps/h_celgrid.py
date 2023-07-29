cc8c2f45452676ac584aa146ee1015d04351387a
Ken Harmon <harmkenn@gmail.com>
2023-07-29 13:26:14 +0200

    Done

--- apps/h_celgrid.py
+++ apps/h_celgrid.py
@@ -57,17 +57,76 @@ def app():
     
     cel_dist = 40050*(90-cel_altitude)/360
     dist = 40050*(90-h_alt)/360*1000
-    st.write(f'sub-Celestial distace from observer: ', cel_dist)
-    st.write(f"{selection}'s Azimuth from 0,0:", cel_azimuth)
-    st.write(f"{selection}'s Altitude from 0,0:", cel_altitude)
+    #st.write(f'sub-Celestial distace from observer: ', cel_dist)
+    #st.write(f"{selection}'s Azimuth from 0,0:", cel_azimuth)
+    #st.write(f"{selection}'s Altitude from 0,0:", cel_altitude)
     sub_cel = zf.polar2LL(0,0,cel_azimuth,cel_dist)
     st.write(f"{selection}'s Sub-Latitude: ", sub_cel[0], f"{selection}'s Sub-Longitude: ", sub_cel[1] )
 
     obloc = zf.revpolar(sub_cel[1],sub_cel[0],float(h_az),dist)
     st.write(f"Observer's Location is Latitude: ", obloc[1], f" Longitude: ", obloc[0])
     
-    st.write(f'here {selection} is: ', zf.sub_cel(selection))
+    #st.write(f'here {selection} is: ', zf.sub_cel(selection))
     
+    
+    sslat = sub_cel[0]  
+    sslon = sub_cel[1]
+    melat = obloc[1]
+    melon = obloc[0]     
+    # map
+    map = folium.Map(location=[0, 0], zoom_start=1)
+    # add tiles to map
+    folium.raster_layers.TileLayer('Open Street Map').add_to(map)
+    folium.raster_layers.TileLayer('Stamen Terrain').add_to(map)
+    # Add custom base maps to folium
+    folium.raster_layers.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Maps',
+            overlay = False,
+            control = True
+        ).add_to(map)
+    folium.raster_layers.TileLayer(
+            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Satellite',
+            overlay = False,
+            control = True
+        ).add_to(map)
+    folium.raster_layers.TileLayer(
+            iles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+            attr = 'Google',
+            name = 'Google Terrain',
+            overlay = False,
+            control = True
+        ).add_to(map)
+
+    # add layer control to show different maps
+    folium.LayerControl().add_to(map)
+    
+    # plugin for mini map
+    minimap = plugins.MiniMap(toggle_display=True)
+
+    # add minimap to map
+    map.add_child(minimap)
+    
+    # add scroll zoom toggler to map
+    plugins.ScrollZoomToggler().add_to(map)
+
+    # add full screen button to map
+    plugins.Fullscreen(position='topright').add_to(map)
+    
+    # add marker to map https://fontawesome.com/v5.15/icons?d=gallery&p=2&m=free
+    sun = folium.features.CustomIcon('Icons/target.png',icon_size=(30,30))
+    folium.Marker(location=[sslat, sslon], color='green', tooltip='SubSolar Point',icon=sun).add_to(map)
+    pal = folium.features.CustomIcon('Icons/paladin.jpg',icon_size=(30,20))
+    folium.Marker(location=[melat, melon], color='green', tooltip='my location',icon=pal).add_to(map)
+    folium.PolyLine([[sslat, sslon],[melat,melon]],tooltip='Azimuth').add_to(map)
+    draw = plugins.Draw()
+    draw.add_to(map)
+    # display map
+    folium_static(map)
+
 
 
         
